ba0db60e0d7201bfc350796296e7685edf824e10
junxiang <junxiang.oriint@gmail.com>
2019-05-08 16:54:43 +0800

    Init: graphql-go

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/xTobu/graphql-go-airtable
+
+go 1.12
+
+require (
+	github.com/gin-gonic/gin v1.4.0
+	github.com/graphql-go/graphql v0.7.8
+	github.com/graphql-go/handler v0.2.3
+)
---
+++ main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"github.com/graphql-go/graphql"
+	"github.com/graphql-go/handler"
+)
+
+var rootQuery = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "RootQuery",
+		Fields: graphql.Fields{
+			"hello": &graphql.Field{
+				Type: graphql.String,
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					return "world", nil
+				},
+			},
+		},
+	},
+)
+
+var todoType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Todo",
+	Fields: graphql.Fields{
+		"id": &graphql.Field{
+			Type: graphql.String,
+		},
+		"text": &graphql.Field{
+			Type: graphql.String,
+		},
+		"done": &graphql.Field{
+			Type: graphql.Boolean,
+		},
+	},
+})
+
+// root mutation
+var rootMutation = graphql.NewObject(graphql.ObjectConfig{
+	Name: "RootMutation",
+	Fields: graphql.Fields{
+		/*
+			curl -g 'http://localhost:8080/graphql?query=mutation+_{createTodo(text:"My+new+todo"){id,text,done}}'
+		*/
+		"createTodo": &graphql.Field{
+			Type:        todoType, // the return type for this field
+			Description: "Create new todo",
+			Args: graphql.FieldConfigArgument{
+				"text": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+
+				// marshall and cast the argument value
+				text, _ := params.Args["text"].(string)
+
+				// figure out new id
+				newID := RandStringRunes(8)
+
+				// perform mutation operation here
+				// for e.g. create a Todo and save to DB.
+				newTodo := Todo{
+					ID:   newID,
+					Text: text,
+					Done: false,
+				}
+
+				TodoList = append(TodoList, newTodo)
+
+				// return the new Todo object that we supposedly save to DB
+				// Note here that
+				// - we are returning a `Todo` struct instance here
+				// - we previously specified the return Type to be `todoType`
+				// - `Todo` struct maps to `todoType`, as defined in `todoType` ObjectConfig`
+				return newTodo, nil
+			},
+		},
+		/*
+			curl -g 'http://localhost:8080/graphql?query=mutation+_{updateTodo(id:"a",done:true){id,text,done}}'
+		*/
+		"updateTodo": &graphql.Field{
+			Type:        todoType, // the return type for this field
+			Description: "Update existing todo, mark it done or not done",
+			Args: graphql.FieldConfigArgument{
+				"done": &graphql.ArgumentConfig{
+					Type: graphql.Boolean,
+				},
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+			},
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				// marshall and cast the argument value
+				done, _ := params.Args["done"].(bool)
+				id, _ := params.Args["id"].(string)
+				affectedTodo := Todo{}
+
+				// Search list for todo with id and change the done variable
+				for i := 0; i < len(TodoList); i++ {
+					if TodoList[i].ID == id {
+						TodoList[i].Done = done
+						// Assign updated todo so we can return it
+						affectedTodo = TodoList[i]
+						break
+					}
+				}
+				// Return affected todo
+				return affectedTodo, nil
+			},
+		},
+	},
+})
+
+// define schema, with our rootQuery and rootMutation
+var schema, _ = graphql.NewSchema(graphql.SchemaConfig{
+	Query:    rootQuery,
+	Mutation: rootMutation,
+})
+
+// Handler initializes the graphql middleware.
+func Handler() gin.HandlerFunc {
+	// Creates a GraphQL-go HTTP handler with the defined schema
+	h := handler.New(&handler.Config{
+		Schema:     &schema,
+		Pretty:     true,
+		GraphiQL:   false,
+		Playground: true,
+	})
+
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+func main() {
+
+	const defaultPort = "9999"
+	r := gin.New()
+	r.POST("/graphql", Handler())
+	r.GET("/graphql", Handler())
+
+	fmt.Printf("http://localhost:%s/graphql?query={hello}\n", defaultPort)
+	r.Run(":" + defaultPort)
+}
